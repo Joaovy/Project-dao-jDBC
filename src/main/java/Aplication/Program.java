9678792d932335940354e88dbb6d52eee101071b
João Antonio <joao.vision.ae@gmail.com>
2025-04-02 14:57:09 -0300

    Implementando os metodos FindByid

--- src/main/java/Aplication/Program.java
+++ src/main/java/Aplication/Program.java
@@ -11,12 +11,9 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.creatSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
