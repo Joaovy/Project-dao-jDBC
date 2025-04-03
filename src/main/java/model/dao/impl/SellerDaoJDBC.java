5d1b40439590f3975abde5605fa2ad691e23337b
João Antonio <joao.vision.ae@gmail.com>
2025-04-03 17:06:08 -0300

    FindByDepartment implementação

--- src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -10,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
@@ -49,7 +52,7 @@ public class SellerDaoJDBC implements SellerDao {
                     "SELECT seller.*,department.Name as DepName "
                             + "FROM seller INNER JOIN department "
                             + "ON seller.DepartmentId = department.Id "
-                            + "WHERE seller.Id = ?");
+                            + "WHERE seller.Id = ? ");
 
             st.setInt(1, id);
             rs = st.executeQuery();
@@ -101,5 +104,57 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public List<Seller> findAll() {
         return List.of();
+    }
+
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE DepartmentId = ? "
+                    + "ORDER BY Name");
+
+            st.setInt(1, department.getId());
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            // crio um map vazio
+            Map<Integer, Department> map = new HashMap<>();
+
+            // testa se veio algum resultado
+            while (rs.next()) {
+
+                // metdo map tenta buscar o id
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                // esquema de verifição se o departamento existe ou não, para que não haja conflito ou duplicidade
+                if (dep == null){
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+
+            }
+                return list;
+
+
+        } catch (SQLException e){
+            throw new DbExceptions(e.getMessage());
+
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+
+
     }
 }
